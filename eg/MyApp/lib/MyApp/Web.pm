478222d036a371d53232bdc054d9d98f04c7717d
hisaichi5518 <info@moe-project.com>
2011-02-12 21:09:08 +0900

    邪魔なの消したり、早くしてみたり。

--- eg/MyApp/lib/MyApp/Web.pm
+++ eg/MyApp/lib/MyApp/Web.pm
@@ -5,22 +5,14 @@ use warnings;
 use parent 'Slug';
 use MyApp::Web::Dispatcher;
 use Tiffany::Text::Xslate;
-use Path::Class qw(dir);
+use FindBin;
 
 sub startup {
     my ($self) = @_;
-    # $self->plugin("Web::MobileAgent" => {});
-    # $self->plugin("Web::MobileEmoji" => {});
-    # $self->plugin("Web::IsMobileIP"  => {});
-
     my $v = Tiffany::Text::Xslate->new({
-        path => [dir("templates")],
-        function => {
-            app => sub { $self }
-        }
+        path => ["$FindBin::Bin/templates"],
     });
     $self->view($v);
-    # $self->dispatcher('MyApp::Web::Dispatcher');
     MyApp::Web::Dispatcher->dispatch($self);
 }
 
