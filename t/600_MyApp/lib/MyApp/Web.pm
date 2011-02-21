28aa7f61de9dff454000493c7779203323e8fa9b
hisaichi5518 <info@moe-project.com>
2011-02-21 17:51:43 +0900

    テストを変更

--- t/600_MyApp/lib/MyApp/Web.pm
+++ t/600_MyApp/lib/MyApp/Web.pm
@@ -4,19 +4,28 @@ use warnings;
 
 use parent 'Slug';
 
-use MyApp::Web::Dispatcher;
-use Tiffany::Text::Xslate;
 use FindBin;
 
 sub startup {
     my ($self) = @_;
     $self->plugin('Test');
-    $self->view(
-        Tiffany::Text::Xslate->new({
-            path => ["$FindBin::Bin/templates"],
-        })
+    $self->plugin('Web::View::Xslate' =>
+        {path => ["$FindBin::Bin/templates"]}
     );
-    MyApp::Web::Dispatcher->dispatch($self);
+    my $r = $self->routes("RSimple");
+    $r->connect('/' => {controller => "Root", action => "index"});
+    $r->connect("/hoge" => {controller => "Root", action => "hoge"});
+    $r->connect("/404" => {controller => "Root", action => "not_found"});
+    $r->connect("/change_status1" => {
+        controller => "Root", action => "change_status1"});
+    $r->connect("/change_status2" => {
+        controller => "Root", action => "change_status2"});
+    $r->connect("/change_status3" => {
+        controller => "Root", action => "change_status3"});
+    $r->connect("/check-env" => {
+        controller => "Root", action => "check_env"});
+    $r->connect("/view/xslate" => {
+        controller => "ViewTest", action => "xslate"});
 }
 
 1;
