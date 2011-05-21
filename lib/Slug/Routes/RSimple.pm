1e4f815e699d1f65e83327f60396a16e37f2a9bc
hisaichi5518 <info@moe-project.com>
2011-05-21 13:31:50 +0900

    RoutesのクラスのPODを簡単に書いた。

--- lib/Slug/Routes/RSimple.pm
+++ lib/Slug/Routes/RSimple.pm
@@ -33,12 +33,46 @@ Slug::Route::RSimple - Router::Simple
 
 =head1 SYNOPSIS
 
+  use Slug::Routes::RSimple;
+  my $r = Slug::Routes::RSimple->new;
+  # my $r = Slug::Routes::RSimple->new(namespace => "MyApp::C");
+  $r->connect('/' => {controller => "Root", action => "index"});
 
 =head1 DESCRIPTION
 
-変わる可能性高い。
+高速なRouter Class である Router::Simple を Slug で使う為のクラス
+
+=head1 ARGS
+
+=head2 namespace
+
+MyApp::Web::Controller 以下にコントローラを置きたくない場合に使う。
+
+これはnew でも指定する事が出来るが、argsで指定したほうが優先される。
+
+=head2 controller
+
+コントローラを指定する。
+
+一番前に「 + 」を付けるとnamespaceが無視される。
+
+controllerが指定されていないと404を返す。
+
+=head2 action
+
+アクションを指定する。
+
+actionが指定されていないと404を返す。
 
 =head1 METHOD
+
+Router::Simple を継承しています。
+
+=head2 dispatch
+
+  $c->dispatch($c);
+
+name_space
 
 =head1 AUTHOR
 
